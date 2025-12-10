f49ebdae745aca5fd3d10ba506db39d86a991949
Jesiel Silveira <jesielms12@gmail.com>
2025-12-10 10:39:10 -0300

    atualização 1.4

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* suas outras opções aqui */
-  
-  eslint: {
-    ignoreDuringBuilds: true, // Ignora warnings e erros de ESLint durante o build
-  },
+  /* suas outras configurações aqui */
 };
 
 export default nextConfig;
